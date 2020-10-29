2ac21716238020479517869a6edcde013a1994c4
Tricia R <triciaroush@live.com>
2020-10-28 23:11:29 -0400

    Added questions for user prompts.

--- index.js
+++ index.js
@@ -1,10 +1,59 @@
+const fs = require("fs").promises;
+const inquirer = require("inquirer");
+const formatREADME = require("./Develop/utils/generateMarkdown");
+const writeFile
+
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "account",
+        message: "What is your GitHub account user name?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "What is the name of your application?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is desired outcome? Will you describe the project?"
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "What type of licensing does the project require?"
+    },
+    {
+        type: "input",
+        name: "dependancies",
+        message: "What command should be run to install dependancies?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "What command should be run to administer a test?"
+    },
+    {
+        type: "input",
+        name: "userInfo",
+        message: "What should the user know about using the repo?"
+    },
+    {
+        type: "input",
+        name: "contribute",
+        message: "What does the user need to know about contributing to the repo?"
+    }
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile("README.md", data) {
 }
 
 // function to initialize program
